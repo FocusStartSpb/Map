a988ce4819460a400b34e6da493804a261f0a1be
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2019-12-16 21:10:54 +0300

    (AKA-3) Добавила swiftLint

--- Map/AppDelegate.swift
+++ Map/AppDelegate.swift
@@ -12,7 +12,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication,
+					didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.rootViewController = UITabBarController()
@@ -21,4 +22,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 }
-
