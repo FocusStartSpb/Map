44af89c39714415733ad7ef6fa478033356b6e63
Arkadiy <dragon_500@mail.ru>
2019-12-17 02:56:48 +0300

    (AKA-5) Создал business layer файлы

---
+++ Map/Business layer/Repository.swift
@@ -0,0 +1,34 @@
+//
+//  Repository.swift
+//  Map
+//
+//  Created by Arkadiy Grigoryanc on 17.12.2019.
+//
+
+struct SmartTarget
+{
+}
+
+protocol ISmartTargetRepository
+{
+	func fetchSmartTargets(
+		_ completion: @escaping SmartTargetsResultCompletion)
+}
+
+final class SmartTargetRepository
+{
+	// MARK: Private methods
+	private var decoderService: IDecoderService
+
+	// MARK: Initialization
+	init(decoderService: IDecoderService) {
+		self.decoderService = decoderService
+	}
+}
+
+extension SmartTargetRepository: ISmartTargetRepository
+{
+	func fetchSmartTargets(
+		_ completion: @escaping SmartTargetsResultCompletion) {
+	}
+}
