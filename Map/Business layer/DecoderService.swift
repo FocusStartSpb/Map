3823d72d0f59b638ecf8f4ca15caf6b5030416f6
belannik <darkmonkz@mail.ru>
2019-12-18 21:55:53 +0300

    (AKA-9) Вынес декодирование в отдельный сервис

--- Map/Business layer/DecoderService.swift
+++ Map/Business layer/DecoderService.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-enum ServiceError: Error
-{
-	case decodingError(Error)
-}
-
 typealias SmartTargetsResult = Result<[SmartTarget], ServiceError>
 
 typealias SmartTargetsResultCompletion = (SmartTargetsResult) -> Void
@@ -21,6 +16,9 @@ protocol IDecoderService
 	func decodeSmartTargets(_ data: Data,
 							_ completion: @escaping SmartTargetsResultCompletion)
 }
+protocol IDecoderGeocoder
+{
+}
 
 final class DecoderService
 {
@@ -29,5 +27,17 @@ final class DecoderService
 extension DecoderService: IDecoderService
 {
 	func decodeSmartTargets(_ data: Data, _ completion: @escaping SmartTargetsResultCompletion) {
+	}
+}
+extension DecoderService: IDecoderGeocoder
+{
+	func decodeGeocode(_ data: Data, completion: @escaping (GeoResults) -> Void) {
+		do {
+			let result = try JSONDecoder().decode(GeoDataWrapper.self, from: data)
+			completion(.success(result))
+		}
+		catch {
+			completion(.failure(.decoding(error)))
+		}
 	}
 }
