16233e7cb7e1bfc037b1639e1f0a4a69bc9c7977
belannik <darkmonkz@mail.ru>
2019-12-28 13:53:41 +0300

    (АКА-7) Класс ячейки

---
+++ Map/Scenes/Smart target list scene/Cell/SmartTargetTableViewCell.swift
@@ -0,0 +1,129 @@
+//
+//  SmartTargetTableViewCell.swift
+//  Map
+//
+//  Created by Антон on 26.12.2019.
+//
+
+import UIKit
+
+protocol ISmartTargetTableViewCell
+{
+	func fillLabels(smartTarget: SmartTarget?)
+}
+
+final class SmartTargetTableViewCell: UITableViewCell
+{
+	private enum FontsForCell
+	{
+		static let timeOfCreationFont = UIFont(name: "Avenir-LightOblique ", size: 10)
+		static let titleLabelFont = UIFont(name: "Cochin-Bold", size: 17)
+		static let addressLabelFont = UIFont(name: "Optima-Regular", size: 17)
+	}
+
+	let containerView = UIView()
+	private let selectedView = UIView()
+	private let timeOfCreationLabel = UILabel()
+	private let titleLabel = UILabel()
+	private let addressLabel = UILabel()
+
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setupContainerView()
+		setupTimeOfCreationLabel()
+		setupTitleLabel()
+		setupAddressLabel()
+		selectionStyle = .none
+	}
+
+	@available (*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: ... Private Methods
+	private func setupContainerView() {
+		self.contentView.addSubview(containerView)
+		self.containerView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			self.containerView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 5),
+			self.containerView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -5),
+			self.containerView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+			self.containerView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -20),
+		])
+		if #available(iOS 13.0, *) {
+			self.containerView.backgroundColor = .systemBackground
+		}
+		else {
+			self.containerView.backgroundColor = .white
+		}
+		self.containerView.layer.cornerRadius = 20
+		self.containerView.layer.shadowOpacity = 0.3
+		self.containerView.layer.shadowOffset = CGSize(width: self.containerView.frame.width,
+													   height: self.containerView.frame.height + 13)
+		self.containerView.layer.borderWidth = 0.07
+	}
+
+	private func setupTimeOfCreationLabel() {
+		self.containerView.addSubview(timeOfCreationLabel)
+		self.timeOfCreationLabel.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			self.timeOfCreationLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
+			self.timeOfCreationLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor, constant: 10),
+			self.timeOfCreationLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
+			self.timeOfCreationLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 7),
+		])
+		self.timeOfCreationLabel.font = FontsForCell.timeOfCreationFont
+		if #available(iOS 13.0, *) {
+			self.timeOfCreationLabel.textColor = .systemGray2
+		}
+		else {
+			self.timeOfCreationLabel.textColor = #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
+		}
+		self.timeOfCreationLabel.textAlignment = .center
+	}
+
+	private func setupTitleLabel() {
+		self.containerView.addSubview(titleLabel)
+		self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			self.titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
+			self.titleLabel.topAnchor.constraint(equalTo: self.timeOfCreationLabel.bottomAnchor),
+			self.titleLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
+			self.titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 10),
+		])
+		self.titleLabel.numberOfLines = 0
+		self.titleLabel.font = FontsForCell.titleLabelFont
+		self.titleLabel.textAlignment = .center
+	}
+
+	private func setupAddressLabel() {
+		self.containerView.addSubview(addressLabel)
+		self.addressLabel.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			self.addressLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
+			self.addressLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 10),
+			self.addressLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
+			self.addressLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 10),
+			self.addressLabel.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -5),
+		])
+		self.addressLabel.numberOfLines = 0
+		self.addressLabel.font = FontsForCell.addressLabelFont
+		self.addressLabel.textAlignment = .center
+		if #available(iOS 13.0, *) {
+			self.addressLabel.textColor = .systemGray
+		}
+		else {
+			self.addressLabel.textColor = .gray
+		}
+	}
+}
+// MARK: - ISmartTargetTableViewCell
+extension SmartTargetTableViewCell: ISmartTargetTableViewCell
+{
+	func fillLabels(smartTarget: SmartTarget?) {
+		self.titleLabel.text = smartTarget?.title
+		self.addressLabel.text = smartTarget?.address
+		self.timeOfCreationLabel.text = "12.12.2012"
+	}
+}
