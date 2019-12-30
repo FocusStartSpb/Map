24ef13ef547d5459c6f1d1b5b28da914086c036a
Arkadiy <dragon_500@mail.ru>
2019-12-30 16:46:48 +0300

    (AKA-39) Добавил контроллер в табБар

--- Map/Scenes/TabBarController/TabBarController.swift
+++ Map/Scenes/TabBarController/TabBarController.swift
@@ -16,11 +16,14 @@ final class TabBarController: UITabBarController
 		let repository = SmartTargetRepository(dataBaseService: dataBaseService)
 		let mapViewController = factory.getMapScene(withRepository: repository)
 		let smartTargetListViewController = factory.getSmartTargetListScene(withRepository: repository)
+		let settingsViewController = factory.getSettingsScene()
 		mapViewController.tabBarItem = UITabBarItem(title: "Map", image: #imageLiteral(resourceName: "icons8-map-64"), tag: 0)
 		smartTargetListViewController.tabBarItem = UITabBarItem(title: "List", image: #imageLiteral(resourceName: "icons8-table-of-content-80"), tag: 1)
+		settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: #imageLiteral(resourceName: "icons-add"), tag: 3)
 		viewControllers = [
 			mapViewController,
 			smartTargetListViewController,
+			settingsViewController,
 		]
 	}
 }
