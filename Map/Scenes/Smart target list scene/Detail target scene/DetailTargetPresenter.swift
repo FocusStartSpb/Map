6ae0908f77074260254fddbb02beed9614fa79d8
belannik <darkmonkz@mail.ru>
2020-01-06 19:42:54 +0300

    (АКА-42) DetailTargetPresenter

---
+++ Map/Scenes/Smart target list scene/Detail target scene/DetailTargetPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  DetailTargetPresenter.swift
+//  Map
+//
+//  Created by Антон on 30.12.2019.
+//
+
+import Foundation
+
+final class DetailTargetPresenter
+{
+	weak var detailTargetViewController: DetailTargetViewController?
+	var dataStore: SmartTargetListDataStore?
+}
