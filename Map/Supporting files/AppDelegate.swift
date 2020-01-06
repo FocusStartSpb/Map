0c69626e834d9961d61dda46a7be9f93b496c725
Arkadiy <dragon_500@mail.ru>
2020-01-06 04:01:14 +0300

    (AKA-49) Добавил проверку нотификация при активации приложения

--- Map/Supporting files/AppDelegate.swift
+++ Map/Supporting files/AppDelegate.swift
@@ -12,6 +12,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate
 {
 	var window: UIWindow?
 
+	let notificationWorker = NotificationWorker(service: NotificationService.default)
+
 	func application(_ application: UIApplication,
 					didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
@@ -24,5 +26,24 @@ final class AppDelegate: UIResponder, UIApplicationDelegate
 
 	func applicationWillResignActive(_ application: UIApplication) {
 		window?.endEditing(true)
+	}
+
+	func applicationDidBecomeActive(_ application: UIApplication) {
+		updateNotifications()
+	}
+}
+
+extension AppDelegate
+{
+	private func updateNotifications() {
+		guard let collection = try? DataBaseService<SmartTargetCollection>().read() else { return }
+		notificationWorker.checkNotifications(for: collection.smartTargets)
+
+		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+			NotificationService.default.getPendingNotificationUIDs { uids in
+				print(uids.count)
+				uids.forEach { print($0) }
+			}
+		}
 	}
 }
