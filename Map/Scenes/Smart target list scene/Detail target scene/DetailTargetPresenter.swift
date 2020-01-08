45a052fd3acd6fc3004be95c06ef98be40cc1e82
belannik <darkmonkz@mail.ru>
2020-01-08 11:46:51 +0300

    (АКА-42) Интерактор теперь Презентер

--- Map/Scenes/Smart target list scene/Detail target scene/DetailTargetPresenter.swift
+++ Map/Scenes/Smart target list scene/Detail target scene/DetailTargetPresenter.swift
@@ -1,5 +1,5 @@
 //
-//  DetailTargetPresenter.swift
+//  DetailTargetInteractor.swift
 //  Map
 //
 //  Created by Антон on 30.12.2019.
@@ -7,8 +7,39 @@
 
 import Foundation
 
+protocol IDetailTargetPresenter
+{
+	func getTarget()
+	func getTitleText() -> String
+	func getAddressText() -> String
+	func getDateOfCreation() -> String
+}
+
 final class DetailTargetPresenter
 {
-	weak var detailTargetViewController: DetailTargetViewController?
-	var dataStore: SmartTargetListDataStore?
+	private let smartTarget: SmartTarget
+
+	init(smartTarget: SmartTarget) {
+		self.smartTarget = smartTarget
+	}
+}
+
+// MARK: - IDetailTargetInteractor
+extension DetailTargetPresenter: IDetailTargetPresenter
+{
+	func getTitleText() -> String {
+		return self.smartTarget.title
+	}
+
+	func getTarget() {
+		print("Below will be a test print")
+	}
+
+	func getDateOfCreation() -> String {
+		return "Date of creation \n 01.01.2020" //ну тип заглушка
+	}
+
+	func getAddressText() -> String {
+		return "\("Address: \n" + (smartTarget.address ?? "not found"))"
+	}
 }
