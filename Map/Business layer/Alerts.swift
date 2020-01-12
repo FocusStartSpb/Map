a8405469fa74e52d8ed0dcb4257dd02bbbcb5e08
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2020-01-12 19:12:39 +0300

    (AKA-40) Добавила файл Alerts

--- Map/Business layer/Alerts.swift
+++ Map/Business layer/Alerts.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import UIKit
+
+enum Alerts
+{
+	private static func showBasicAlert(on vc: UIViewController, with title: String, message: String) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		vc.present(alert, animated: true)
+	}
+
+	static func showDeletePinAlert(on vc: UIViewController) {
+		showBasicAlert(on: vc, with: "Внимание", message: "Повторное действие удалит сохраненную локацию")
+	}
+}
