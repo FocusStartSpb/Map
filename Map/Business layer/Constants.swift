9aa1e37e169b6059e34bda529b0554409dec17cf
belannik <darkmonkz@mail.ru>
2020-01-21 00:53:19 +0300

    (АКА-72) Добавил константы

--- Map/Business layer/Constants.swift
+++ Map/Business layer/Constants.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Arkadiy Grigoryanc on 16.01.2020.
 //
+// swiftlint:disable nesting
 
 import CoreLocation
 import UIKit
@@ -14,7 +15,6 @@ enum Constants
 
 	enum MeasurementSystem
 	{
-		// swiftlint:disable:next nesting
 		enum Symbol
 		{
 			static let imperial = "фт"
@@ -73,6 +73,42 @@ enum Constants
 				return .light
 			}
 		}()
+	}
+
+	enum ScreenProperties
+	{
+		static let width = UIScreen.main.bounds.width
+		static let height = UIScreen.main.bounds.height
+	}
+
+	enum Colors
+	{
+		static let selectedCellBackgroundColorInDarkMode = #colorLiteral(red: 0.3045190282, green: 0.3114352223, blue: 0.3184640712, alpha: 1)
+		static let tableViewBackgroundColorInDarkMode = #colorLiteral(red: 0.2204069229, green: 0.2313892178, blue: 0.253805164, alpha: 1)
+		static let viewBackgroundColorInDarkMode = #colorLiteral(red: 0.2204069229, green: 0.2313892178, blue: 0.253805164, alpha: 1)
+		static let navigationBarTintColorInDarkMode = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+		static let selectedCellBackgroundColorInLightMode = #colorLiteral(red: 0.7502671557, green: 0.7502671557, blue: 0.7502671557, alpha: 1)
+		static let viewBackgroundColorInLightMode = UIColor.white
+		static let tableViewBackgroundColorInLightMode = #colorLiteral(red: 0.9871620841, green: 0.9871620841, blue: 0.9871620841, alpha: 1)
+		static let navigationBarTintColorInLightMode = UIColor.white
+	}
+
+	enum Fonts
+	{
+		enum ForCells
+		{
+			static let timeOfCreation = UIFont.systemFont(ofSize: 15, weight: .light)
+			static let titleLabel = UIFont.systemFont(ofSize: 20, weight: .bold)
+			static let addressLabel = UIFont.systemFont(ofSize: 20, weight: .regular)
+		}
+
+		enum ForDetailScreen
+		{
+			static let titleTextView = UIFont.systemFont(ofSize: 25, weight: .semibold)
+			static let dateOfCreationLabel = UIFont.systemFont(ofSize: 20, weight: .light)
+			static let addressLabel = UIFont.systemFont(ofSize: 20, weight: .light)
+			static let attendanceLabels = UIFont.systemFont(ofSize: 20, weight: .light)
+		}
 	}
 
 	static let activityIndicatorStyle: UIActivityIndicatorView.Style = {
