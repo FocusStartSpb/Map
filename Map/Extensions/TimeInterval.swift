0da35cb63cabc545911b8a17984789f725fcda6c
belannik <darkmonkz@mail.ru>
2020-01-21 00:54:01 +0300

    (АКА-72) Расширение для тайм интервала

---
+++ Map/Extensions/TimeInterval.swift
@@ -0,0 +1,19 @@
+//
+//  TimeInterval.swift
+//  Map
+//
+//  Created by Anton on 21.01.2020.
+//
+
+import Foundation
+
+extension TimeInterval
+{
+	func stringFromTimeInterval() -> String {
+		let time = NSInteger(self)
+		let seconds = time % 60
+		let minutes = (time / 60) % 60
+		let hours = (time / 3600)
+		return String(format: "%0.2d:%0.2d:%0.2d.", hours, minutes, seconds)
+	}
+}
